e9ab53e246a7db22500bf99b812c6fd56599a55b
MIN <minbh060807@gmail.com>
2026-07-10 04:52:44 +0900

    chore: security

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -3,7 +3,14 @@ from datetime import datetime, timedelta
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-SECRET_KEY = "secret key"
+import os
+
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY 환경변수가 설정되지 않았습니다.")
+
+
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
